3d577b372eb7d2573de837b8539a973fb0bdbad0
bobboyms <$bob250387>
2023-07-08 21:49:13 -0300

    adjust

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ def els_process():
     els = InformationRetrievalTest(collection_name=Collection.Trec_covid, arch=Arch.ELS)
     els.index_documents(url="http://127.0.0.1:9200/")
     els.retrival_documents()
-    pd.DataFrame(els.recall_precision_metrics()).to_json("els_fscore.json")
-    pd.DataFrame(els.ncdg_metrics()).to_json("els_ndcg.json")
+    pd.DataFrame(els.recall_precision_metrics()).to_json("els_trec_covid_fscore.json")
+    pd.DataFrame(els.ncdg_metrics()).to_json("els_trec_covid_ndcg.json")
 
 
 def sira_process():
     sira = InformationRetrievalTest(collection_name=Collection.Trec_covid, arch=Arch.SIRA)
     sira.index_documents(url="http://127.0.0.1:8080/nir")
     sira.retrival_documents()
-    pd.DataFrame(sira.recall_precision_metrics()).to_json("sira_fscore.json")
-    pd.DataFrame(sira.ncdg_metrics()).to_json("sira_ndcg.json")
+    pd.DataFrame(sira.recall_precision_metrics()).to_json("sira_trec_covid_fscore.json")
+    pd.DataFrame(sira.ncdg_metrics()).to_json("sira_trec_covid_ndcg.json")
 
 
 if __name__ == '__main__':
